e6f1ed7c40b14567bf808e0d0a3ee9febc13834f
sfernandezhr <sfernandezhr@gmail.com>
2022-03-31 18:33:03 -0500

    5-print_comb2.py

---
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,20 @@
+for i in range (0,99):
+    #print(f"{i}",end=", ")
+    print(str(i).zfill(2),end=", ")
+print ("99")
+
+
+
+
+
+
+
+
+'''
+paso 1. definir un rango del 0 al 100.
+paso 2. imprimir horizontalmente los numeros separados por comas y un espacio entre cada uno
+paso3. imprimir los numeros en orden ascendente con dos digitos
+paso4. el ultimo numero debe estar seguido por una nueva linea
+
+
+'''

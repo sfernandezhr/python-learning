67cd6242f0145db296daf67874b670727e544229
sfernandezhr <sfernandezhr@gmail.com>
2022-04-18 15:52:07 -0500

    files

---
+++ python-if_else_loops_functions/9-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+print_last_digit = __import__('9-print_last_digit').print_last_digit
+
+r=print_last_digit(98)
+print(r)
+r=print_last_digit(0)
+print(r)
+r= print_last_digit(-1024)
+print(r)
---
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,4 @@
+def print_last_digit(number):
+
+    number = str(number)
+    return number[-1]

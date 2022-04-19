2925083c20cc2a70f07d77355a5a2436d0a240e7
sfernandezhr <sfernandezhr@gmail.com>
2022-04-19 18:52:28 -0500

    files

---
+++ python-if_else_loops_functions/palindrome.py
@@ -0,0 +1,20 @@
+
+word='a'
+def isPalindrome(word):
+    return word == word[::-1]
+    
+
+
+    
+
+
+    # if len(word)==0 or len(word)==1:
+    #     return True
+    # else:
+    #     return False
+
+
+
+
+package= isPalindrome(word)
+print(package)
